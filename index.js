2e952de433347344882a0f7a49c11dd2a2631941
keeponbeat <mrkm.toru@gmail.com>
2015-05-19 19:31:44 +0900

    index.js changed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var http = require('http');
 var APP_ID = process.env.APP_ID;
+var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('こにゃにゃちわ！！ \n');
-}).listen(5000);
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('あなたのAPP_IDは、'+APP_ID+' です。');
+}).listen(8124);
 
-console.log('Server running at port:5000',"APP_ID is "+APP_ID);
+console.log('Server running at port 8124');
