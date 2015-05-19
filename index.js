f76405f570922ea84388793d2448da7a64485650
keeponbeat <mrkm.toru@gmail.com>
2015-05-19 19:04:03 +0900

    change port to 5000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var APP_ID = process.env.APP_ID;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!! \n');
-}).listen(3333);
+  res.end('こにゃにゃちわ！！ \n');
+}).listen(5000);
 
-console.log('Server running at port:3333',"APP_ID is"+APP_ID);
+console.log('Server running at port:5000',"APP_ID is "+APP_ID);
