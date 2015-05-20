d5aefd6968cca6256fd95c755ac0b9e1e7d9da9a
keeponbeat <mrkm.toru@gmail.com>
2015-05-20 16:57:24 +0900

    getへの対応-index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var APP_ID = process.env.APP_ID;
 var http = require('http');
+var url = require('url');
 
 http.createServer(function (req, res) {
+  var year = url.parse(req.url,true).query;
   res.writeHead(200);
-  res.end("こにちわ！APP_IDは"+APP_ID+"です。\n");
+  res.end("こにちわ！APP_IDは"+APP_ID+"です。\n" + year + "年度のデータです。");
 }).listen(process.env.PORT || 5000);
