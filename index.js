e7a3c1e26cade76a0a80e84980c64038c16fb0b9
keeponbeat <mrkm.toru@gmail.com>
2015-05-20 21:37:41 +0900

    index.jsを

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var APP_ID = process.env.APP_ID;
-var server = require('http').createServer().listen(process.env.PORT || 5000);
+var server = require('http').createServer();
 var io = require('socket.io')(server);
 
+server.listen(process.env.PORT || 5000);
+
 io.on('connection',function(socket){
+    console.log('server bound');
 
     socket.on('request_data',function(year){
-    socket.emit('data',{data:year});
+        socket.emit('data',{data:year});
     });
 
     socket.on("close", function() {
-    console.log("connection close")
+        console.log("connection close")
   })
 })
