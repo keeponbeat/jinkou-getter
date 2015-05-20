af0cc65870121b7bde39ca952b649137d350bf3e
keeponbeat <mrkm.toru@gmail.com>
2015-05-20 19:09:37 +0900

    add ws node module

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 var APP_ID = process.env.APP_ID;
-var net = require('net');
-var url = require('url');
+var WebSocketServer = require('ws').Server;
+var wsServer = new WebSocketServer({ port: process.env.PORT || 5000});
 
-var server = net.createServer(function (c){
-  console.log('client connected');
-  c.on('end',function(){
-    console.log('client end');
-  });
-  c.on('data',function(data){
-    c.pipe(c);
-  });
-});
-
-server.listen( process.env.PORT || 5000, function(){
-    console.log('server bound');
-});
+wsServer.on('connection',function(c){
+    c.on('message',function(message){
+        c.send(message);
+    });
+})
