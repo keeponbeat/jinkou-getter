dab8150e001ec887e3a62472171eceac73e349a9
keeponbeat <mrkm.toru@gmail.com>
2015-05-20 16:59:25 +0900

    少し修正 index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var url = require('url');
 
 http.createServer(function (req, res) {
-  var year = url.parse(req.url,true).query;
+  var query = url.parse(req.url,true).query;
   res.writeHead(200);
-  res.end("こにちわ！APP_IDは"+APP_ID+"です。\n" + year + "年度のデータです。");
+  res.end("こにちわ！APP_IDは"+APP_ID+"です。\n" + query.year + "年度のデータです。");
 }).listen(process.env.PORT || 5000);
