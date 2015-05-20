4d45f1716aadce6e02760cd15ffdeaf27fff1d11
keeponbeat <mrkm.toru@gmail.com>
2015-05-20 19:57:11 +0900

    heartbeatの間違いを修正

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var WebSocketServer = require('ws').Server;
 var wsServer = new WebSocketServer({ port: process.env.PORT || 5000});
 
 wsServer.on('connection',function(c){
+    //heartbeat
     var id = setInterval(function() {
-        ws.send(JSON.stringify(new Date()), function() {  })
-        }, 1000);
+    c.send(JSON.stringify(new Date()), function() {  })
+    }, 1000);
+
     c.on('message',function(message){
     c.send(message);
     });
+
     c.on("close", function() {
     console.log("websocket connection close")
     clearInterval(id)
