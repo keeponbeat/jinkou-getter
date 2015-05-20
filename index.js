4f546f0e447dc07642ee6e823a56c40de8865275
keeponbeat <mrkm.toru@gmail.com>
2015-05-20 19:54:04 +0900

    add heartbeat to index.js

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ var WebSocketServer = require('ws').Server;
 var wsServer = new WebSocketServer({ port: process.env.PORT || 5000});
 
 wsServer.on('connection',function(c){
+    var id = setInterval(function() {
+        ws.send(JSON.stringify(new Date()), function() {  })
+        }, 1000);
     c.on('message',function(message){
-        c.send(message);
+    c.send(message);
     });
+    c.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
 })
