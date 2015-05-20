966da4a504763d7bbbc6c8292c7dd97c65938140
keeponbeat <mrkm.toru@gmail.com>
2015-05-20 21:52:48 +0900

    index.jsを

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var APP_ID = process.env.APP_ID;
 var server = require('http').createServer();
 var io = require('socket.io')(server);
-
 server.listen(process.env.PORT || 5000);
+
+io.configure('production', function(){
+  io.set('transports', ['websocket','xhr-polling']);
+});
 
 io.on('connection',function(socket){
     console.log('server bound');
